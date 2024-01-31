17167e8c5aa177173aca06088aecacc69d6b58ae
Muhammad Akhta <akhtacaesar@gmail.com>
2024-01-31 23:56:06 +0700

    Minor Updates

--- Coursemology S100P/Missions S102/Libraries & Modules/L2 Password Generator.py
+++ Coursemology S100P/Missions S102/Libraries & Modules/L2 Password Generator.py
@@ -1,30 +1,12 @@
 import random
 
 def generate_password():
-
+    
     length = int(input("Password Length: "))
-    lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
-    uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    digits = '0123456789'
-    symbols = '!@#$%^&*()-_=+[]{}|;:,.<>?/'
-
+    
     password = ""
-    all_characters = lowercase_letters + uppercase_letters + digits + symbols
-
-    if length == 4:
-        password = ''.join(random.choice(all_characters) for x in range(4))
-    elif length == 3:
-        password = ''.join(random.choice(all_characters) for x in range(3))
-    elif length == 2:
-        password = ''.join(random.choice(all_characters) for x in range(2))
-    elif length == 1:
-        password = random.choice(all_characters)
-    else:
-        password = ''.join(random.choice(all_characters) for x in range(length))
-
-    password_list = list(password)
-    random.shuffle(password_list)
-    password = ''.join(password_list)
+    for y in range(length):
+        password += chr(random.randint(33, 126))
 
     return password
 
