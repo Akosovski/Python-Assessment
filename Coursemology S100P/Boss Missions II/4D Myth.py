dff261f09e5f9d4d62c2fd6aa962b9a25d387735
Muhammad Akhta <akhtacaesar@gmail.com>
2024-02-07 15:30:33 +0700

    Minor Update

--- Coursemology S100P/Boss Missions II/4D Myth.py
+++ Coursemology S100P/Boss Missions II/4D Myth.py
@@ -1,50 +1,63 @@
 import random
 
 def generate_lucky_number():
-    return ''.join(str(random.randint(0, 9)) for _ in range(4))
+    lucky_num = ""
+    for i in range(4):
+        lucky_num += str(random.randint(0, 9))
+    return lucky_num
 
-def calculate_winnings(bet_number, lucky_number):
-    return 3000 if bet_number == lucky_number else 0
+def set_bet_number():
+    bet_num = ""
+    for i in range(4):
+        bet_num += str(random.randint(0, 9))
+    return bet_num
 
-def first_betting_technique():
+def check_win(bet_number, lucky_number):
+    jackpot = 3000
+    if bet_number == lucky_number:
+        return jackpot
+    else:
+        return 0
+
+def first_bet_technique():
     total_bets = 0
     total_winnings = 0
 
-    for _ in range(10000):
-        bet_number = ''.join(str(random.randint(0, 9)) for _ in range(4))
+    for x in range(10000):
+        bet_number = ""
+        for y in range(4):
+            bet_number += str(random.randint(0, 9))
         lucky_number = generate_lucky_number()
 
         total_bets += 1
-        total_winnings += calculate_winnings(bet_number, lucky_number)
+        total_winnings += check_win(bet_number, lucky_number)
+    print(total_bets)
+    return total_winnings / total_bets
 
-    expected_return = total_winnings / total_bets
-    return expected_return
-
-def second_betting_technique():
+def second_bet_technique():
     total_bets = 0
     total_winnings = 0
 
-    for _ in range(10000):
-        bet_number = ''.join(str(random.randint(0, 9)) for _ in range(4))
+    for x in range(10000):
+        bet_number = ""
+        for y in range(4):
+            bet_number += str(random.randint(0, 9))
         lucky_number = generate_lucky_number()
 
-        # Avoid numbers from the lucky number in the prior round
         avoid_numbers = set(lucky_number)
         while any(digit in avoid_numbers for digit in bet_number):
-            bet_number = ''.join(str(random.randint(0, 9)) for _ in range(4))
+            bet_number = set_bet_number()
 
         total_bets += 1
-        total_winnings += calculate_winnings(bet_number, lucky_number)
+        total_winnings += check_win(bet_number, lucky_number)
 
-    expected_return = total_winnings / total_bets
-    return expected_return
+    print(total_bets)
+    return total_winnings / total_bets
 
 def lottery():
-    expected_return_first_technique = first_betting_technique()
-    expected_return_second_technique = second_betting_technique()
+    first_bet_result = first_bet_technique()
+    second_bet_result = second_bet_technique()
 
-    return f"{expected_return_first_technique:.2f} {expected_return_second_technique:.2f}"
+    return f"{first_bet_result:.2f} {second_bet_result:.2f}"
 
-# Example usage
-result = lottery()
-print(result)
+print(lottery())
--- Coursemology S100P/Boss Missions II/Roulette Master.py
+++ Coursemology S100P/Boss Missions II/Roulette Master.py
@@ -1,28 +1,27 @@
 import random
 
-def roulette():
-    # Question 1: Expected earning for each bet
+def earning_per_bet():
     total_bets = 0
-    total_winnings = 0
+    total_win = 0
 
-    for _ in range(10000):
+    for x in range(10000):
         result = random.randint(0, 36)
         bet_amount = 1
 
         if result == 0:
-            total_winnings -= bet_amount
+            total_win -= bet_amount
         elif result % 2 == 0:
-            total_winnings += bet_amount
+            total_win += bet_amount
 
         total_bets += bet_amount
 
-    expected_earning_bet = total_winnings / total_bets
+    return total_win / total_bets
 
-    # Question 2: Expected earning for each round of Donald's strategy
+def earning_per_round():
     total_rounds = 0
     total_wealth = 0
 
-    for _ in range(100000):
+    for y in range(100000):
         wealth = 0
         bet_amount = 1
         rounds = 0
@@ -44,10 +43,9 @@ def roulette():
         total_wealth += wealth
         total_rounds += rounds
 
-    expected_earning_strategy = total_wealth / total_rounds
+    return total_wealth / total_rounds
 
-    return f'{expected_earning_bet:.2f} {expected_earning_strategy:.2f}'
+def roulette():
+    return f"{earning_per_bet():.2f} {earning_per_round():.2f}"
 
-# Example usage
-result = roulette()
-print(result)
+print(roulette())
