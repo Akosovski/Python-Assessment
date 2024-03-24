0591d5c985cf86d52611083fd4ff8d2f64c16d59
Muhammad Akhta <akhtacaesar@gmail.com>
2024-03-24 10:09:28 +0700

    Minor Addition

--- Coursemology S100P/testing.py
+++
@@ -1,8 +0,0 @@
-def monotonic_checker(numbers):
-    increasing = all(numbers[i] <= numbers[i + 1] for i in range(len(numbers) - 1))
-    decreasing = all(numbers[i] >= numbers[i + 1] for i in range(len(numbers) - 1))
-
-    return increasing or decreasing
-
-print(monotonic_checker([2,4,6,8]))
-print(monotonic_checker([2,4,1,8]))
---
+++ Hangman.py
@@ -0,0 +1,35 @@
+import random
+
+print("Hangman Game!")
+
+words_list = ["apple", "orange", "banana", "laptop", "monitor"]
+word = words_list[random.randint(0, len(words_list) - 1)]
+tries = 5
+
+user_input = []
+for i in word:
+  user_input.append("_")
+
+answer = []
+for i in word:
+  answer.append(i)
+
+while tries != 0:
+
+  if user_input == answer:
+    print("\nYou won! The word is: " + ''.join(user_input))
+    break
+  
+  print("\n" + ' '.join(user_input))
+  print(f"You have {tries} tries left!")
+  user_guess = input("Which letter would you like to try? ")
+    
+  if user_guess in answer:
+    for i in range(len(answer)):
+      if user_guess == answer[i]:
+        user_input[i] = user_guess
+  else:
+    tries -= 1
+
+if tries == 0:
+  print("\nYou lose! The word is: " + ''.join(answer))
