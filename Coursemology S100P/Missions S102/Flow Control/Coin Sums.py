0b990cf0f81741a325e6878081c08b91abab9255
Muhammad Akhta <akhtacaesar@gmail.com>
2026-01-01 01:10:56 +0700

    Minor Changes

--- Coursemology S100P/Missions S102/Flow Control/Coin Sums.py
+++ Coursemology S100P/Missions S102/Flow Control/Coin Sums.py
@@ -1,20 +1,14 @@
 def coinSums(x):
-    # Define the available coin denominations
-    coins = [1, 5, 10, 20, 50, 100]
+    amount = 0
+    for one in range(0, x//100 + 1):
+        for fifty in range(0, x//50 + 1):
+            for twenty in range(0, x//20 + 1):
+                for ten in range(0, x//10 + 1):
+                    for five in range(0, x//5 + 1):
+                        for single in range(0, x//1 + 1):
+                            if (one * 100 + fifty * 50 + twenty * 20 + ten * 10 + five * 5 + single * 1) == x:
+                                amount = amount + 1
+    return(amount)
 
-    # Initialize an array to store the number of ways for each amount from 0 to x
-    ways = [0] * (x + 1)
-
-    # There is one way to make change for 0 cents (no coins)
-    ways[0] = 1
-
-    # Iterate through each coin denomination
-    for coin in coins:
-        # Update the ways array for each amount from coin to x
-        for amount in range(coin, x + 1):
-            ways[amount] += ways[amount - coin]
-
-    return ways[x]
-
-print(coinSums(10))
+print(coinSums(12))
 print(coinSums(50))
--- Playground.py
+++ Playground.py
@@ -1,6 +1,22 @@
-num = int(input("Enter a number: "))
 
-if not num == 15:
-    print("Osten")
-else:
-    print("You")
+def copycat(name):
+    duplicates = []
+    name = name.split()
+    duplicate = False
+
+    for i in name:
+        if i.lower() in duplicates:
+            duplicate = True
+            break
+        else:
+            duplicates.append(i.lower())
+
+    if duplicate:
+        return "Duplicates"
+    else:
+        return "No Duplicates"
+
+name = "HI HI"
+name2 = "Liao Alex Alex"
+
+print(copycat(name))
